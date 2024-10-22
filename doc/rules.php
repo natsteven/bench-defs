a98da7277a2552625ff380adee2a7e0e648b7aa6
Jan Strejček <xstrejc@fi.muni.cz>
2024-10-22 14:25:12 +0000

    comment on global variables, suggested by Simmo.

--- doc/rules.php
+++ doc/rules.php
@@ -242,7 +242,7 @@ as a reaction to the community feedback after the first competition
             (see 6.5.6 of <a href="https://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf">C11</a> 
             standard) stored in a program variable. The variable can be of a pointer type or 
             of a compound type containing a pointer. The variable does not have to be in the 
-            current scope, it can be on the call stack.
+            current scope, it can be global or on the call stack.
           </li>
           <li>If some pointer in a tracked block points to another block (again, not necessarily 
             to the beginning of the block) or to the first address after this block, this pointed 
